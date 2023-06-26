ce7cde633c67cf9ce0768ff911bb846992e5c878
니나니모 <carl2005@naver.com>
2023-06-26 18:48:41 +0900

    .

---
+++ UE_SnakeTest/Source/UE_SnakeTest/UE_SnakeTestGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE_SnakeTestGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE_SNAKETEST_API AUE_SnakeTestGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
